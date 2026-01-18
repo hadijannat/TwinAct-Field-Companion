6947b3720d527f942155c0ff8486b7b51ec6fc5a
hadijannat <aeroshariati@gmail.com>
2026-01-18 21:09:19 +0100

    Initial Commit

---
+++ TwinAct Field Companion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TwinAct Field Companion
+//
+//  Created by Hadi Jannat on 18.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TwinAct Field Companion/TwinAct_Field_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwinAct_Field_CompanionApp.swift
+//  TwinAct Field Companion
+//
+//  Created by Hadi Jannat on 18.01.26.
+//
+
+import SwiftUI
+
+@main
+struct TwinAct_Field_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
